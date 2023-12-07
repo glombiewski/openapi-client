13fa882f08e6505963bc967687778e019d626909
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2023-12-07 22:19:53 +0100

    refactoring of generate.py

--- .generation/generate.py
+++ .generation/generate.py
@@ -4,6 +4,9 @@
 Generator for the OpenAPI client.
 '''
 
+from __future__ import annotations
+import argparse
+from dataclasses import dataclass
 from pathlib import Path
 from urllib import request
 from urllib.error import URLError
@@ -14,106 +17,223 @@ import shutil
 import subprocess
 import sys
 import time
+from typing import Literal
 
-# Set `DEV_RUN=1` to run the generator in development mode.
-DEV_RUN: bool = int(os.environ.get('DEV_RUN', 0)) == 1
 
-# Read configuration from `config.ini`.
-config = configparser.ConfigParser()
 CWD = Path('.generation/')
-config.read(CWD / 'config.ini')
 
-# Python package name
-PACKAGE_NAME: str = config['package']['name']
-PACKAGE_VERSION: str = config['package']['version']
-PACKAGE_URL: str = config['package']['url']
 
-# Specify backend version
-GE_BACKEND_TAG: str = config['input']['backendTag']
+def eprint(*args, **kwargs):
+    '''Print to stderr.'''
+    print(*args, file=sys.stderr, **kwargs)
 
-if DEV_RUN:
-    print("Running in development mode.", file=sys.stderr)
 
-# Generate the openapi.json file.
-#
-# Imitating manually fetching it, e.g.
-#
-# ```bash
-# wget http://localhost:3030/api/api-docs/openapi.json -O - \
-#   | python -m json.tool --indent 2 > .generation/input/openapi.json
-# ```
-#
-if not DEV_RUN:
-    print("Starting Geo Engine backend.", file=sys.stderr)
+class ProgramArgs(argparse.Namespace):
+    '''Typed command line arguments.'''
+    language: Literal['python', 'typescript']
+    fetch_spec: bool
+    build_container: bool
+
+    @staticmethod
+    def parse_arguments() -> ProgramArgs:
+        '''Parse command line arguments.'''
+        parser = argparse.ArgumentParser(
+            description='Create a client for the Geo Engine API.')
+        parser.add_argument('--no-spec-fetch', dest='fetch_spec', action='store_false',
+                            required=False, default=True)
+        parser.add_argument('--no-container-build', dest='build_container', action='store_false',
+                            required=False, default=True)
+        parser.add_argument('language', choices=['python', 'typescript'],
+                            type=str)
+
+        parsed_args: ProgramArgs = parser.parse_args()
+        return parsed_args
+
+
+@dataclass
+class ConfigArgs():
+    '''Typed config.ini arguments.'''
+    # Backend version
+    ge_backend_tag: str
+
+    # General
+    github_url: str
+
+    # Python package name
+    python_package_name: str
+    python_package_version: str
+
+    # TypeScript package name
+    typescript_package_name: str
+    typescript_package_version: str
+
+    @staticmethod
+    def parse_config() -> ConfigArgs:
+        '''Parse config.ini arguments.'''
+        parsed = configparser.ConfigParser()
+        parsed.read(CWD / 'config.ini')
+
+        return ConfigArgs(
+            ge_backend_tag=parsed['input']['backendTag'],
+            github_url=parsed['general']['githubUrl'],
+            python_package_name=parsed['python']['name'],
+            python_package_version=parsed['python']['version'],
+            typescript_package_name=parsed['typescript']['name'],
+            typescript_package_version=parsed['typescript']['version'],
+        )
+
+
+def fetch_spec(*, ge_backend_tag: str) -> None:
+    '''
+    Generate the openapi.json file.
+
+    Imitating manually fetching it, e.g.
+
+    ```bash
+    wget http://localhost:3030/api/api-docs/openapi.json -O - \
+      | python -m json.tool --indent 2 > .generation/input/openapi.json
+    ```
+    '''
+    eprint("Starting Geo Engine backend.")
 
     ge_process = subprocess.Popen(
         [
             "podman", "run",
-                "--rm", # remove the container after running
-                "--network=host", # port 8080 by default
-                # "-p", "3030:8080",
-                f"quay.io/geoengine/geoengine:{GE_BACKEND_TAG}",
+            "--rm",  # remove the container after running
+            "--network=host",  # port 8080 by default
+            # "-p", "3030:8080",
+            f"quay.io/geoengine/geoengine:{ge_backend_tag}",
         ],
     )
 
-    for _ in range(90):
-        print("Requesting `openapi.json`….", file=sys.stderr)
+    for _ in range(180):  # <3 minutes
+        eprint("Requesting `openapi.json`….")
         try:
             with request.urlopen(
                 "http://localhost:8080/api/api-docs/openapi.json",
-                 timeout=10,
+                timeout=10,
             ) as w:
                 api_json = json.load(w)
 
             with open(CWD / "input/openapi.json", "w", encoding='utf-8') as f:
                 json.dump(api_json, f, indent=2)
 
-            print("Stored `openapi.json`.", file=sys.stderr)
+            eprint("Stored `openapi.json`.")
             break
-        except URLError as e:
-            pass # try again
-        time.sleep(1) # 1 second
+        except URLError as _e:
+            pass  # try again
+        time.sleep(1)  # 1 second
 
-    print("Stopping Geo Engine backend.", file=sys.stderr)
+    eprint("Stopping Geo Engine backend.")
     ge_process.kill()
 
-# Build the patched generator image.
-if not DEV_RUN:
+
+def build_container():
+    '''Build the patched generator image'''
     subprocess.run(
         [
             "podman", "build",
-                "-t", "openapi-generator-cli:patched",
-                CWD,
+            "-t", "openapi-generator-cli:patched",
+            CWD,
         ],
         check=True,
     )
 
-# Remove the test directory, since it will not be overwritten by the generator.
-if os.path.isdir("test"):
-    shutil.rmtree("test")
 
-# Run the generator.
-subprocess.run(
-    [
-        "podman", "run",
+def clean_test_dirs():
+    '''Remove the test directory, since it will not be overwritten by the generator.'''
+    for lang in ['python', 'typescript']:
+        test_path = Path(lang) / 'test'
+        if os.path.isdir(test_path):
+            shutil.rmtree(test_path)
+
+
+def generate_python_code(*, package_name: str, package_version: str, package_url: str):
+    '''Run the generator.'''
+    subprocess.run(
+        [
+            "podman", "run",
             "--network=host",
-            "--rm", # remove the container after running
+            "--rm",  # remove the container after running
             "-v", f"{os.getcwd()}:/local",
             f"--env-file={CWD / 'override.env'}",
             # "docker.io/openapitools/openapi-generator-cli:v7.0.1",
             "openapi-generator-cli:patched",
-                "generate",
-                    "-i", f"{'/local' / CWD / 'input/openapi.json'}",
-                    "-g", "python",
-                    "--additional-properties=" + ",".join([
-                        "useOneOfDiscriminatorLookup=true",
-                        # "generateSourceCodeOnly=true",
-                        f"packageName={PACKAGE_NAME}",
-                        f"packageVersion={PACKAGE_VERSION}",
-                        f"packageUrl={PACKAGE_URL}",
-                    ]),
-                    "--enable-post-process-file",
-                    "-o", "/local/",
-    ],
-    check=True,
-)
+            "generate",
+            "-i", f"{'/local' / CWD / 'input/openapi.json'}",
+            "-g", "python",
+            "--additional-properties=" + ",".join([
+                "useOneOfDiscriminatorLookup=true",
+                # "generateSourceCodeOnly=true",
+                f"packageName={package_name}",
+                f"packageVersion={package_version}",
+                f"packageUrl={package_url}",
+            ]),
+            "--enable-post-process-file",
+            "-o", "/local/python/",
+        ],
+        check=True,
+    )
+
+
+def generate_typescript_code(*, package_name: str, package_version: str, package_url: str):
+    '''Run the generator.'''
+    subprocess.run(
+        [
+            "podman", "run",
+            "--network=host",
+            "--rm",  # remove the container after running
+            "-v", f"{os.getcwd()}:/local",
+            f"--env-file={CWD / 'override.env'}",
+            # "docker.io/openapitools/openapi-generator-cli:v7.0.1",
+            "openapi-generator-cli:patched",
+            "generate",
+            "-i", f"{'/local' / CWD / 'input/openapi.json'}",
+            "-g", "typescript-fetch",
+            "--additional-properties=" + ",".join([
+                "useOneOfDiscriminatorLookup=true",
+                # "generateSourceCodeOnly=true",
+                f"packageName={package_name}",
+                f"packageVersion={package_version}",
+                f"packageUrl={package_url}",
+            ]),
+            "--enable-post-process-file",
+            "-o", "/local/typescript/",
+        ],
+        check=True,
+    )
+
+
+def main():
+    '''The entry point of the program'''
+    args = ProgramArgs.parse_arguments()
+    config = ConfigArgs.parse_config()
+
+    if args.fetch_spec:
+        fetch_spec(ge_backend_tag=config.ge_backend_tag)
+
+    if args.build_container:
+        build_container()
+
+    clean_test_dirs()
+
+    if args.language == 'python':
+        generate_python_code(
+            package_name=config.python_package_name,
+            package_version=config.python_package_version,
+            package_url=config.github_url,
+        )
+    elif args.language == 'typescript':
+        generate_typescript_code(
+            package_name=config.typescript_package_name,
+            package_version=config.typescript_package_version,
+            package_url=config.github_url,
+        )
+    else:
+        raise RuntimeError(f'Unknown language {args.language}.')
+
+
+if __name__ != '__main__':
+    raise RuntimeError('This module should not be imported.')
+
+main()

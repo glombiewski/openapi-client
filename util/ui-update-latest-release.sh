58e3f9851b73f099fe478cbb1231b5750e4fa782
Michael Mattig <michael.m@ttig.de>
2024-03-25 15:57:06 +0100

    increment version

---
+++ util/ui-update-latest-release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# FOR DEVELOPMENT USE ONLY
+#
+# update the openapi-client to the latest release version in the ui
+
+set -e
+
+version=$(awk -F " = " '/version/ {print $2; exit}' .generation/config.ini)
+
+cd ../geoengine-ui
+
+npm uninstall @geoengine/openapi-client
+npm install @geoengine/openapi-client@${version}
+
+cd -
